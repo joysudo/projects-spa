80d395a915be9c896155e51ab5a27db926ffe898
Joy Su <joysu2008@gmail.com>
2026-03-22 13:35:07 -0400

    card content + adjust font/color again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,196 @@
 <header>
-    <h1>JOY SU(DO)</h1>
+    <h1>Joy Su(do)</h1>
     <p>Tall person, short name, long list</p>
 </header>
 <main>
-    <section class="column">
-        <h2 class="column-header">INITIATIVES</h2>
-        <div class="project-card">
-            <h3>This is an h3 element hooray.</h3>
-            <p>This is text that goes underneath that h3 element. This is filler text. I am writing filler text right now. This will show what it looks like once the page is filled with yap. Filler text. Consider this area filled. With text.</p>
-            <img src="https://ceiling.hackclub.com/ceiling.jpg" alt=""/>
-            <a href="https://byjoysu.com" target="_blank">This is a link.</a>
-        </div>
-    </section>
-    <section class="column">
-        <h2 class="column-header">ART</h2>
-    </section>
-    <section class="column">
-        <h2 class="column-header">WRITING</h2>
-    </section>
+    <div class="grid">
+        <section class="column">
+            <h2 class="column-header">Student Advocacy/Education</h2>
+            <div class="project-card">
+                <h3>Iowa House File 658/Senate File 86</h3>
+                <p>I'd done a lot of advocacy leading up to this point (I hosted my first protest in middle school, and I led our freshman student council). I wanted to take student advocacy to the next level. This is legislation that friends and I developed as a position statement, got sponsored by republican and democratic senators, passed subcommittee, passed committee, and brought to debate in the House and Senate.</p>
+                <p>The work I offer to take on often revolves around a lot of writing and talking. Think pitches, outreach emails, grant proposals, etc. I spent hours crawling the Iowa Legislature website, looking at voting records, and rewriting email templates for each target. This was years before I realized mass emailers exist for a reason.</p>
+                <div>
+                    <img src="https://cdn.hackclub.com/019d1600-587f-74b1-aa09-1a8260696321/Screenshot%202026-03-22%20104017.png" alt="screenshot of dozens of sent emails"/>
+                    <p>(Subject lines remain the same, but contents vary wildly.)</p>
+                </div>
+                <a href="https://www.legis.iowa.gov/docs/publications/LGI/90/HF658.pdf" target="_blank">Read the text of the bill</a>
+                <a href="https://docs.google.com/document/d/1aDBNogBameubbESAtvewgihN5EQeK5vPrb5w1EKq8CQ/edit?tab=t.0" target="_blank">Read an example FAQ I wrote</a>
+            </div>
+            <div class="project-card">
+                <h3>Middle School Programs @ Pi515</h3>
+                <p>I joined Pi515 when I was feeling tired of government. Chatting with legislators feels tokenistic after your third day of it. Pi515 was a partner in a mentorship I did with John Deere, shadowing software engineers and developing games in Python. I sent a cold email from the back of my sixth period classroom and begged to be let in. </p>
+                <a href="https://replit.com/@byjs/MazeGame">Example goofy game</a>
+                <p>It worked! I joined when the 2023 middle school initiative was just in its initial planning phase, and helped take it from concepts of a plan to execution. Accessibility for a program like this means maintaining a sufficient student:mentor ratio and providing students with transportation home, which involved calling and coordinating. I developed curriculum and pacing. We launched in 3 underserved middle schools in the DMPS district, teaching ~50 kids block coding for 4 months.</p>
+                <a href="https://www.facebook.com/watch/?v=220355900724310&rdid=633eHf7jgYGoVmpU" target="_blank">See one student's final project</a>
+                <a href="https://www.facebook.com/watch/?v=976427706957673&rdid=iU1CgAnq1WXgo5M5" target="_blank">See another student's final project</a>
+                <p>I've done other work with Pi515, proceding to win the Girl's Entrepreneurship Summit and having returned this year to work on the AI Toolkit. Ask me for a reference.</p>
+                <img src="https://www.businessrecord.com/wp-content/uploads/2023/05/image.png.webp" alt="Joy presenting business pitch"/>
+                <a href="https://www.businessrecord.com/pi515-celebrates-girls-entrepreneurship-summit-honors-john-pappajohn/">Read an article where I look clipped</a>
+            </div>
+            <div class="project-card">
+                <h3>Girls Who Code Student Leadership Council</h3>
+                <p>Another example of a project where writing proposals is a huge part of the gig. I was part of the inaugural Girls Who Code Student Leadership Council, proposing the idea for a pilot mentorship program (connecting career women in computer science with low-income early college students) and helping to draft its proposal.</p>
+                <img src="https://cdn.hackclub.com/019d166e-aad8-7ddf-862b-d7d54e0396f7/girlswhocode.png" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Campfire RTP</h3>
+                <p>I was the head organizer of Campfire RTP, a game jam for teens in the Research Triangle Park. See our winners:</p>
+                <img src="https://cdn.hackclub.com/019d163d-d6cf-7972-bbaa-7d413bddc842/HPIM2465.JPG" alt=""/>
+                <a href="https://campfire.hackclub.com/rtp">Website</a>
+            </div>
+            <div class="project-card">
+                <h3>Sucrates</h3>
+                <p>In a development I did not expect, I now teach AP African American Studies on Youtube.</p>
+                <img src="https://cdn.hackclub.com/019d1646-782b-7fad-9943-97f3a0add86a/image.jpg" alt=""/>
+                <a href="https://www.youtube.com/watch?v=bQUaCf-qujM&t">Watch a video</a>
+            </div>
+        </section>
+        <section class="column">
+            <h2 class="column-header">Illustration</h2>
+            <div class="project-card">
+                <h3>Unmute! w/ Mental Health Americaa</h3>
+                <p>This was a project I developed from concept to execution. It's in this section because the other one wasn overflowing. Coordinated 10 performers, got sponsorship from Mental Health America, got a venue at Cabarrus Arts Council. I also helped develop the visual identity of the project, creating all of our marketing materials. Used a lot of halftones in Adobe Illustrator.</p>
+                <img src="https://cdn.hackclub.com/019d1639-921e-701c-a35e-5fe2648ad4b4/IMG_8306.jpeg" alt=""/>
+                <img src="https://cdn.hackclub.com/019d1600-69c3-793e-b7c7-ca044a5f0588/6df67bb8-1.png" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Prototype</h3>
+                <p>I attended Prototype, a hardware hackathon in San Jose, and built Stack the Circus with Emma and Ryan. Following the event, I built the official recap website and gallery of winners. Sveltekit.</p>
+                <img src="https://cdn.hackclub.com/019d1647-d3b9-7999-9143-fc5182cb552f/Screenshot%202026-03-22%20082852.png" alt=""/>
+                <a href="https://prototype-w87m.vercel.app/">See the website</a>
+                <!-- <img src="https://cdn.hackclub.com/019d1647-de79-77e3-b820-b0079c3a89fc/Screenshot%202026-03-22%20082928.png" alt=""/> -->
+                <img src="https://cdn.hackclub.com/019d164a-864d-722c-8aae-b667ae725f85/IMG_0154.png" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Clearing the Air</h3>
+                <p>Data analysis project I created and illustrated for Kode With Klossy.</p>
+                <img src="https://cdn.hackclub.com/019d1647-bd74-762f-a071-e425caca25e4/clearingtheair.png" alt=""/>
+                <a href="https://joysu20083.wixsite.com/clearingtheair">See the website</a>
+            </div>
+            <div class="project-card">
+                <h3>What?</h3>
+                <p>What is that? I have no idea.</p>
+                <img src="https://cdn.hackclub.com/019d164f-621e-79e5-83d0-51435fe78b09/Screenshot%202026-03-22%20120943.png" alt=""/>
+                <a href="https://docs.google.com/document/d/1edhOnAOpH1F8vTHUd0dS5GoyUrXs83TYbwDky-OlLLA/edit?usp=sharing">That's crazy bro</a>
+            </div>
+            <div class="project-card">
+                <h3>Campfire S'more</h3>
+                <p>A mini game jam I planned in two days. Thanks KL!</p>
+                <img src="https://cdn.hackclub.com/019d1657-477d-742c-9014-152993b7949d/Untitled_Artwork(5).png" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Milkyway Merch</h3>
+                <p>Proposed 25+ merch ideas, voted on budget options, and illustrated PCB.</p>
+                <img src="https://cdn.hackclub.com/019d166b-8967-79dc-84d3-f9760c74daed/image%20(1).png" alt=""/>
+            </div>
+        </section>
+        <section class="column">
+            <h2 class="column-header">Writing/Hobbies</h2>
+            <div class="project-card">
+                <h3>Assorted Accolades</h3>
+                <p>Fun fact: I lived alone for a year in seventh grade and and studied creative writing at a nearby magnet school. I've kept it up since.</p>
+                <p>I've won various awards in writing, including:</p>
+                <ul>
+                    <li>Silver keys for poetry and short stories from the Scholastic Art and Writing Awards</li>
+                    <li>Nomination from PTA Reflections for cultural commentary essay</li>
+                    <li>Publication in various magazines, such as ALOCASIA, the lickety~split, the Augment Review, and more</li>
+                </ul>
+                <a href="https://alocasia.org/2022/12/21/joy-su-fertilizer/">Read an example poem</a>
+            </div>
+            <div class="project-card">
+                <h3>Debate</h3>
+                <p>I used to compete regularly in Lincoln Douglas debate. I'm no longer an active competitor due to financial reasons (debate is expensive if your new public school doesn't fund it), but I teach. During my time as a competitor, I was lucky enough to win various national tournaments, including Apple Valley MinneApple and the Millard West Fred Robertson Invitational.</p>
+                <a href="https://www.tabroom.com/index/results/team_results.mhtml?id1=1193351&id2=">See my record</a>
+            </div>
+            <div class="project-card">
+                <h3>Mèimei #2</h3>
+                <p>I've been teaching one of my little sisters to draw. She's making amazing progress. It is so time-consuming.</p>
+                <img src="https://cdn.hackclub.com/019d1658-272f-7bdf-a0fd-b9cf4061ec87/image0.png" alt=""/>
+                <img src="https://cdn.hackclub.com/019d1658-314d-724c-8990-6f2a357674c6/image1.png" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Mèimei #1</h3>
+                <p>I've been teaching my other little sister how to make video games. She's making amazing progress. It is so time-consuming.</p>
+                <img src="https://img.itch.zone/aW1nLzI2MTMzNzg4LnBuZw==/315x250%23c/VC8T3w.png" alt=""/>
+                <a href="https://janicedtea.itch.io">Play her games (& leave a comment)</a>
+            </div>
+            <div class="project-card">
+                <h3>Yum</h3>
+                <p>I make incredible cookies. Side effect of having two little sisters & having lived alone for a while.</p>
+                <img src="https://cdn.hackclub.com/019d1682-6b6f-7e33-843f-21594e46d9cd/IDG_20260222_105104_511.JPG" alt=""/>
+            </div>
+            <div class="project-card">
+                <h3>Cattatime Studios</h3>
+                <p>I met Kat Wang and Kaylee Dinh at Parthenon, the world's largest all-girls hackathon. We've been making things together ever since. This includes our original coding virtual pet, Cattatime, a platformer puzzle game named Oh My Gatos, and even a program website in progress?</p>
+                <img src="https://cdn.hackclub.com/019d1685-4744-7611-b49c-43b02c5a1a3f/image%20(2).png" alt=""/>
+                <img src="https://cdn.hackclub.com/019d1689-4bcc-7f9a-8700-2c061caf54a9/cattatime3dthin.png" alt=""/>
+                <a href="https://joysudo.itch.io/">Play Oh My Gatos</a>
+                <a href="https://github.com/joysudo/cattatime">Download Cattatime</a>
+            </div>
+        </section>
+    </div>
+    <div class="project-card bottom">
+        <h3>Here's what I'm most comfortable in:</h3>
+        <ul>
+            <li>Web development: SvelteKit (TS/JS), React (TypeScript/JavaScript), HTML, CSS</li>
+            <li>Game development: Godot Engine (GDScript, 2D game creation)</li>
+            <li>Assorted languages: Python (especially data analysis with Pandas/Scikit), Java (games/puzzles)</li>
+            <li>Art: vector (Adobe Illustrator, Inkscape), illustration (Procreate), pixel (Aseprite)</li>
+        </ul>
+        <h3>And what I'm wanting to learn:</h3>
+        <ul>
+            <li>Semi-realism (doing studies under Gato Wang this summer)</li>
+            <li>Proficiency in backend development that extends past saving Cattatime data to a json</li>
+            <li>How to perfect the brownie recipe, now that I've got cookies secured</li>
+        </ul>
+    </div>
 </main>
+
 <footer>
     <p>Want to find more information? Check out my <a href="https://byjoysu.com" target="_blank">personal website</a> or <a href="https://github.com/joysudo" target="_blank">Github</a>.</p>
 </footer>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap'); 
+    @import url('https://fonts.googleapis.com/css2?family=Molle:ital@1&display=swap');
 
     :global(body) {
         margin: 0;
         text-align: center;
-        background-color: color-mix(in srgb, darkolivegreen 15%, mintcream);
+        background-color: color-mix(in srgb, darkolivegreen 20%, snow);
     }
 
     * {
         box-sizing: border-box;
         margin: 0;
         padding: 0;
-        font-family: 'Nanum Pen Script', monospace;
+        font-family: monospace;
         color: darkolivegreen;
         line-height: 1.2rem;
     }
     
+    h1, h2, h3 {
+        font-family: 'Molle';
+    }
+
     header {
         display: inline-block;
-        background-color: mintcream;
+        background-color: snow;
         border: 2px solid darkolivegreen;
         padding: 2rem 4rem;
         border-radius: 4rem 1rem;
         margin-top: 2rem;
         margin-bottom: 3rem;
         box-shadow: -0.5rem 0.5rem 0 darkolivegreen;
-        animation: hover 2s ease-in-out infinite alternate;
+        animation: float 2s ease-in-out infinite alternate;
     }
 
-    @keyframes hover {
+    @keyframes float {
         from { transform: translateY(0); }
         to { transform: translateY(1rem); }
     }
     
-    main {
+    .grid {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         gap: 1rem;
@@ -77,37 +209,32 @@
         padding: 1rem;
         z-index: 67;
         background: darkolivegreen;
-        color: mintcream;
+        color: snow;
         text-align: center;
         border-radius: 6767px;
-        transition: transform 0.4s ease-in-out, box-shadow 0.4s ease-in-out;
-    }
-
-    .column-header:hover {
-        transform: translate(0.25rem, -0.25rem);
-        box-shadow: -0.25rem 0.25rem 0 color-mix(in srgb, darkolivegreen 50%, mintcream);
     }
 
     .project-card {
         display: flex;
         flex-direction: column;
-        background: mintcream;
+        background: snow;
         border: 2px solid darkolivegreen;
         border-radius: 1rem;
         padding: 1rem;
         gap: 0.5rem;
         text-align: left;
-        box-shadow: 0 0 0 color-mix(in srgb, darkolivegreen 25%, mintcream);
+        box-shadow: 0 0 0 color-mix(in srgb, darkolivegreen 50%, snow);
         transition: transform 0.4s ease-in-out, box-shadow 0.4s ease-in-out;
     }
 
-    .project-card:hover {
+    .project-card:hover:not(.bottom) {
         transform: translate(0.5rem, -0.35rem);
-        box-shadow: -0.35rem 0.5rem 0 color-mix(in srgb, darkolivegreen 50%, mintcream);
+        box-shadow: -0.35rem 0.5rem 0 color-mix(in srgb, darkolivegreen 50%, snow);
     }
 
     .project-card h3 {
         font-size: 1.5rem;
+        font-weight: normal;
     }
     
     .project-card img {
@@ -119,7 +246,7 @@
     .project-card a {
         display: block;
         background-color: sienna;
-        color: mintcream;
+        color: snow;
         padding: 0.5rem 1rem;
         border-radius: 1rem;
         font-weight: bold;
@@ -131,6 +258,15 @@
         background-color: saddlebrown;
     }
 
+    ul {
+        margin-left: 1rem;
+    }
+    .bottom {
+        margin: 2rem 2vw;
+        margin-bottom: 3rem;
+        animation: float 5s ease-in-out infinite alternate;
+    }
+
     footer {
         padding: 4rem;
         margin-top: 2rem;
@@ -139,7 +275,7 @@
     }
 
     footer p {
-        color: mintcream;
+        color: snow;
     }
 
     footer a {
