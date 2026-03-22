fa74dd9a38b84789d2e5a3ef67ed63f975d86375
Joy Su <joysu2008@gmail.com>
2026-03-22 09:02:43 -0400

    header

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<header>
+    <h1>JOY SU(DO)</h1>
+    <p>Tall person, short name, long list</p>
+</header>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Lexend+Exa:wght@200;400;700&display=swap');
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Space Mono', monospace;
+    }
+    header {
+        display: inline-block;
+        border: 3px solid black;
+        padding: 2rem 4rem;
+        border-radius: 4rem 1rem;
+    }
+    
+    header h1 {
+        font-family: 'Lexend Exa', sans-serif !important;
+    }
+
+</style>
