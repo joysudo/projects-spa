02d688bd4d09bf1776a13d622d23de5a50766b2d
Joy Su <joysu2008@gmail.com>
2026-03-22 09:11:46 -0400

    column headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,17 @@
     <h1>JOY SU(DO)</h1>
     <p>Tall person, short name, long list</p>
 </header>
+<main>
+    <section class="column">
+        <div class="column-header">INITIATIVES</div>
+    </section>
+    <section class="column">
+        <div class="column-header">ART</div>
+    </section>
+    <section class="column">
+        <div class="column-header">WRITING</div>
+    </section>
+</main>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Lexend+Exa:wght@200;400;700&display=swap');
@@ -20,7 +31,32 @@
     }
     
     header h1 {
-        font-family: 'Lexend Exa', sans-serif !important;
+        font-family: 'Lexend Exa', sans-serif;
     }
 
+    main {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 1rem;
+        margin: 0 auto;
+        align-items: start;
+    }
+
+    .column {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    .column-header {
+        position: sticky;
+        padding: 1rem;
+        z-index: 67;
+        background: darkolivegreen;
+        color: whitesmoke;
+        text-align: center;
+        border-radius: 6767px;
+        top: 10vh;
+        font-family: 'Lexend Exa', sans-serif;
+    }
 </style>
